8a19ddfee149a8041c8a4835ae63999db5bf0f18
Meidan Nasi <meidanasi94@gmail.com>
2020-02-17 16:49:25 +0200

    admin funcs

--- routes/items.js
+++ routes/items.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 var bodyParser = require('body-parser');
-
-
-
+var fs = require('fs');
 
 router.use(bodyParser.json()); // for parsing application/json
 //router.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
@@ -19,51 +17,79 @@ router.get('/', function(req, res, next) {
   // query the data base
   db.query(sql, function(err, rows, fields) {
     if (err) {
-      res.status(500).send({ error: 'Something failed!' })
+      return res.status(500).send({ error: 'Something failed!' })
     }
+    console.log(rows)
     res.json(rows)
   })
 console.log("Client asked for items")
 });
 
 
-
-
 /*post method for create an item*/
 router.post('/create', function(req, res, next) {
-  var title = req.body.title;
-  var desc = req.body.desc;
-  var price = req.body.price;
-  var img = req.body.img;
+  const id = req.body.id;
+  const title = req.body.title;
+  const desc = req.body.desc;
+  const price = req.body.price;
+  const quantity = req.body.quantity;
+  const url = req.body.url;
   
   console.log("Client asked to add an item", req.body)
-  var sql = `INSERT INTO innodb.Items (title, desc, price, img) VALUES ("${title}", "${desc}", "${price}", "${img}")`;
-  console.log("query to db is:"+ sql)
+
+  const query ="INSERT INTO `innodb`.`Items` (`id`, `title`, `desc`, `price`, `img`, `quantity`) VALUES"+`('${id}', '${title}', '${desc}', '${price}', '${url}' , '${quantity}' )`
+  console.log("query to db is:"+ query)
 
   // query the data base
-  db.query(sql, function(err, result) {
+  db.query(query,function(err, result) {
     if(err) {
-      res.status(500).send({ error: 'Something failed!' })
+      return res.status(500).send({ error: 'Something failed!' })
     }
-    res.json({'status': 'success', id: result.insertId})
+    res.json({'status': 'success'})
   })
 });
 
-
-
 /*rest api to delete record from mysql database*/
 router.delete('/delete', function(req, res, next) {
-
   console.log("Client asked to delete" , req.query.id)
-  var sql = `DELETE FROM employee WHERE id=${req.query.id}`
+  var sql = `DELETE FROM Items WHERE id=${req.query.id}`
   console.log("query to db is:"+ sql)
   
   // query the data base
   db.query(sql, function(err, result) {
     if(err) {
-      res.status(500).send({ error: 'Something failed!' })
+      return res.status(500).send({ error: 'Something failed!' })
     }
     res.json({ 'status': 'success' })
+  })
+});
+
+/*get method for fetch single product*/
+router.get('/id', function(req, res, next) {
+  var id = req.query.id;
+  console.log(id)
+
+  var sql = `SELECT * FROM Items WHERE id=${id}`;
+  db.query(sql, function(err, row, fields) {
+    if(err) {
+      return res.status(500).send({ error: 'Something failed!' })
+    }
+    res.json(row[0])
+  })
+});
+
+/*put method for update product*/
+router.put('/update/id', function(req, res, next) {
+  console.log(req)
+  var id = req.body.id;
+  var quantity = (req.body.quantity) ;
+
+  var sql = `UPDATE Items SET quantity="${quantity}" WHERE id=${id}`;
+  db.query(sql, function(err, result) {
+    if(err) {
+      return res.status(500).send({ error: 'Something failed!' })
+    }
+    res.json({'status': 'success'})
   })
 });
 
